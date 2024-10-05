332cd285a6c274c66c7ae48af8543f86094bdd85
Đào Văn Quyền <80147846+quyendv@users.noreply.github.com>
2024-10-05 17:39:59 +0700

    feat: install power_toys

--- windows/setup_window_custom.ps1
+++ windows/setup_window_custom.ps1
@@ -37,6 +37,7 @@ Write-Start -msg "Installing Scoop's packages"
     scoop install <#Web browser #> googlechrome # firefox brave
     scoop install <# Tool #> ripgrep fzf # which alacritty neofetch obs-studio
     scoop install <# Coding #> neovim windows-terminal vscode nodejs python postman # gcc openjdk
+    scoop install <# Windows #> powertoys
     # scoop install <# Runtime lib #> vagrant
     # Start-Process -Wait powershell -verb runas -ArgumentList "scoop install DejaVuSanMono-NF-Mono vcredist-aio nonportable/virtualbox-np docker"
 Write-Done

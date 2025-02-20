4533a8a92d0228a7777f93b2fa5ecbf695ed8926
Đào Văn Quyền <80147846+quyendv@users.noreply.github.com>
2025-02-21 00:04:51 +0700

    feat: docker-setup.sh - ubuntu 22.04

--- cicd/setup-docker/docker-setup.sh
+++ cicd/setup-docker/docker-setup.sh
@@ -7,10 +7,11 @@ sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Add Docker repository
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 
 # Install Docker
 sudo apt install -y docker-ce
